5c976879667cd77717e4761bcef6d1de73808620
Kaspars <kaspars.skels@gmail.com>
2012-03-18 14:18:32 +0100

    Adding a tool for sending words

---
+++ src/device/sphinx/main.cpp
@@ -0,0 +1,45 @@
+
+#include <connection.hpp>
+#include <core.pb.h>
+#include <sphinx.pb.h>
+#include <log.hpp>
+
+#include <iostream>
+#include <sstream>
+
+
+int main(int argc, char* argv[]) {
+  // init log to the terminal printouts 
+  Log::registerConsumer(Log::ToCoutConsumer());
+
+  Connection connection("localhost", 19292);
+  connection.connect(); // move to constructor?
+
+  if (!connection.state())
+    return -1;
+
+  SphinxMessage::Words words;
+  words.add_word("go");
+  words.add_word("forward");
+  words.add_word("10");
+  words.add_word("meters");
+
+  SphinxMessage sphinx;
+  *(sphinx.mutable_words()) = words;
+
+  UbiMessage::Payload payload;
+  payload.set_name("SphinxMessage");
+  std::stringstream ss;
+  sphinx.SerializeToOstream(&ss);
+  payload.set_data(ss.str());
+
+  UbiMessage ubi;
+  ubi.set_type(UbiMessage::DATAFLOW);
+  *(ubi.mutable_payload()) = payload;
+
+  connection.send(ubi);
+  connection.disconnect();
+
+  return 0;
+}
+
