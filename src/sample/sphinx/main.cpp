b252470b99aa971c266ba740b9a0ae694a15372e
Kaspars <kaspars.skels@gmail.com>
2012-04-22 18:02:38 +0200

    Some adds in the sphinx stuff

--- src/sample/sphinx/main.cpp
+++ src/sample/sphinx/main.cpp
@@ -1,63 +1,50 @@
 
-#include <pocketsphinx.h>
+#include <boost/thread.hpp>
 
-#define MODELDIR "/opt/sphinx/pocket/0.7/share/pocketsphinx/model"
+struct Ctx {
+  
+};
 
-int
-main(int argc, char *argv[])
+struct Foo {
+  Foo(Ctx* ctx) : _ctx(ctx) {};
+  void operator()(){
+    std::cout << "..." << std::endl;
+  };
+  Ctx* _ctx;
+};
+
+struct Processer processer  {
+  Processer(Ctx* ctx) : _ctx(ctx) {};
+  void operator()(){
+    std::cout << "..." << std::endl;
+  };
+  Ctx* _ctx;
+};
+
+struct Sender {
+  Sender(Ctx* ctx) : _ctx(ctx) {};
+  void operator()() {
+    std::cout << "running sender.." << std::endl;
+  }
+  Ctx* _ctx;
+};
+
+int main(int argc, char* argv[])
 {
-  ps_decoder_t *ps;
-  cmd_ln_t *config;
-  FILE *fh;
-  char const *hyp, *uttid;
-  int16 buf[512];
-  int rv;
-  int32 score;
+  Ctx ctx;
 
-  config = cmd_ln_init(NULL, ps_args(), TRUE,
-                       "-hmm", MODELDIR "/hmm/en_US/hub4wsj_sc_8k",
-                       "-lm", MODELDIR "/lm/en/turtle.DMP",
-                       "-dict", MODELDIR "/lm/en/turtle.dic",
-                       NULL);
-  if (config == NULL)
-    return 1;
-  ps = ps_init(config);
-  if (ps == NULL)
-    return 1;
+  Foo foo(&ctx);
+  boost::thread thread(foo);
 
-  fh = fopen("goforward.raw", "rb");
-  if (fh == NULL) {
-    perror("Failed to open goforward.raw");
-    return 1;
-  }
+  Sender sender(&ctx);
+  boost::thread sender_thread(sender);
 
-  rv = ps_decode_raw(ps, fh, "goforward", -1);
-  if (rv < 0)
-    return 1;
-  hyp = ps_get_hyp(ps, &score, &uttid);
-  if (hyp == NULL)
-    return 1;
-  printf("Recognized: %s\n", hyp);
+  Processer processer(&ctx);
+  boost::thread 
 
-  fseek(fh, 0, SEEK_SET);
-  rv = ps_start_utt(ps, "goforward");
-  if (rv < 0)
-    return 1;
-  while (!feof(fh)) {
-    size_t nsamp;
-    nsamp = fread(buf, 2, 512, fh);
-    rv = ps_process_raw(ps, buf, nsamp, FALSE, FALSE);
-  }
-  rv = ps_end_utt(ps);
-  if (rv < 0)
-    return 1;
-  hyp = ps_get_hyp(ps, &score, &uttid);
-  if (hyp == NULL)
-    return 1;
-  printf("Recognized: %s\n", hyp);
+  thread.join(); 
+  sender_thread.join();
 
-  fclose(fh);
-  ps_free(ps);
   return 0;
 }
 
